005c18ad29556de4c433ba45a4e9efb5b268ab02
SushantKhanal <dreamerking114@gmail.com>
2024-01-08 10:14:53 -0600

    user entity created

---
+++ spring-security-client/src/main/java/com/example/springsecurityclient/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.springsecurityclient.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    @Column(length = 60)
+    private String password;
+    private String role;
+    private boolean enabled = false;
+}
+
+
