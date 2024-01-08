609fd9a96fff420e9dc4527ffc282d2979132754
SushantKhanal <dreamerking114@gmail.com>
2024-01-08 10:20:40 -0600

    user repositories and service classes initialised

---
+++ spring-security-client/src/main/java/com/example/springsecurityclient/service/UserService.java
@@ -0,0 +1,4 @@
+package com.example.springsecurityclient.service;
+
+public interface UserService {
+}
---
+++ spring-security-client/src/main/java/com/example/springsecurityclient/service/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.springsecurityclient.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
