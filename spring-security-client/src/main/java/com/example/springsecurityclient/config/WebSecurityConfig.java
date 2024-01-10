14585c4db2105e67ab864b8aa6c9ff2438ceea04
SushantKhanal <dreamerking114@gmail.com>
2024-01-10 17:17:08 -0600

    cors, csrf, whitelisted url

--- spring-security-client/src/main/java/com/example/springsecurityclient/config/WebSecurityConfig.java
+++ spring-security-client/src/main/java/com/example/springsecurityclient/config/WebSecurityConfig.java
@@ -2,17 +2,35 @@ package com.example.springsecurityclient.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 ////@EnableWebSecurity annotation implicitly includes the @Configuration annotation
 //@Configuration can be used in any Spring application for various purposes. In contrast,
 //@EnableWebSecurity is used only in the context of a web application where Spring Security is implemented.
 @EnableWebSecurity
+@Configuration
 public class WebSecurityConfig {
+    private static final String[] WHITE_LIST_URLS = {
+            "/hello",
+            "/register"
+    };
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(WHITE_LIST_URLS).permitAll()
+                );
+        return httpSecurity.build();
     }
 }
--- spring-security-client/src/main/java/com/example/springsecurityclient/controller/RegistrationController.java
+++ spring-security-client/src/main/java/com/example/springsecurityclient/controller/RegistrationController.java
@@ -4,9 +4,10 @@ import com.example.springsecurityclient.entity.User;
 import com.example.springsecurityclient.event.RegistrationCompleteEvent;
 import com.example.springsecurityclient.model.UserModel;
 import com.example.springsecurityclient.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +19,26 @@ public class RegistrationController {
     @Autowired
     private ApplicationEventPublisher publisher;
     @PostMapping("/register")
-    public String registerNewUser(@RequestBody UserModel userModel) {
+    public String registerNewUser(@RequestBody UserModel userModel, final HttpServletRequest request) {
         User user = userService.registerUser(userModel);
         publisher.publishEvent(new RegistrationCompleteEvent(
                 user,
-                "url" //later we can replace this with actual url
+                applicationUrl(request)
         ));
         return "Success";
+    }
+
+    @GetMapping("/hello")
+    public String helloUser() {
+        return "Hello New User";
+    }
+
+    private String applicationUrl(HttpServletRequest request) {
+        //constructs and returns a String representing the base URL of the application
+        return "http://" + request.getServerName() + ":" + request.getServerPort() +
+                request.getContextPath();
+        //This gets the context path of the application. The context path is the portion of the URL that indicates the
+        // base path of the application. If your application is hosted at the root context, this will be an empty string.
+        // If it’s not, it will be something like /myapp.
     }
 }
